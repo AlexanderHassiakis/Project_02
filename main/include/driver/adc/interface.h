d27fcfb878752683d2648d58d50fe344b66100b9
alexander <alexander.hassiakis@gmail.com>
2026-04-24 15:02:05 +0200

    Gpio work

--- main/include/driver/adc/interface.h
+++ main/include/driver/adc/interface.h
@@ -1,10 +1,6 @@
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdlib.h>
-
-#include "driver/gpio.h"
-#include "driver/utils.h"
-
 #pragma once
+
+
+
+
 
---
+++ main/include/driver/gpio/interface.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include <cstdint.h>
+
+
+namespace include::driver::
+{
+	
+} // namespace name
+
+
+
+
