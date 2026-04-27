4638dc893af222d47d6e6c6d1915f1276df9198d
alexander <alexander.hassiakis@gmail.com>
2026-04-27 10:31:28 +0200

    added new Stub code

--- main/include/driver/serial/stub.h
+++ main/include/driver/serial/stub.h
@@ -4,123 +4,106 @@
 #include <cstd
 #include <chrono>
 
-
 namespace include::driver
 {
-	class stub final: public Inteface
+	class stub final : public Inteface
 	{
-        public:
-			/**
-			 * @brief Constructor.
-			 */
-			Stub() noexcept;
+	public:
+		/**
+		 * @brief Constructor.
+		 */
+		Stub() noexcept
+            : myDataAvailable{false}
+          	,myConnetionStatus{false}
+			,
+		{
+            return 0U; // placeholder
+		}
 
-			/**
-			 * @brief destructor
-			 * 			Deletes to release memory
-			 */
-			~Stub() noexcept = default :
+		/**
+		 * @brief destructor
+		 * 			Deletes to release memory
+		 */
+		~Stub() noexcept
+		{
 
-			/**
-			 * @brief Initializes the Uart communication
-			 *
-			 */
-			void init_uart() noexcept override {
+		}
 
-            std::printf("UART Initialized!\n")
-          	}
+		/**
+		 * @brief Initializes the Uart communication
+		 *
+		 * @return True on success, false on failure.
+		 *
+		 */
+		bool init() noexcept override
+		{
+			std::printf("Starts initelizing UART!\n");
+			std::printf("UART Connected!\n");
+			myConnetionStatus{true};
+		}
 
-			/**
-			 * @brief sending commands/data
-			 *
-			 */
-			 void send_data(std::uint8_t msg*) noexcept override
-			{
-				return 0U; //placeholder
-
+		/**
+		 * @brief sending commands/data
+		 *
+		 */
+		void send(const char *msg) noexcept override
+		{	//Check if there is a connection.
+			if (!myConnectionStatus){return 0U;}
+			 // Print message if valid.
+        	if (nullptr != msg) { std::printf("%s", msg,"Will be sent!\n"); }
+			myDataAvailable{true};
 			}
 
-			/**
-			 * @brief Sends commands
-			 * 
-			 * @param [in] command to be sent via uart.
-			 */
-			void sendCommand(std::uint8_t command) noexcept
-			{
-				if (command == 0u;){return 0U;} // checks the command to see that its something there.
-				
-				if (!connectionStatus) {return 0U;} // check to see if the connection is up. 
-
-				return 0u; // placeholder
+		void send(const std::uint8_t *buf, std::uint16_t bufLen) noexcept override
+		{	//Check if there is a connection.
+			if (!myConnectionStatus){return 0U;}
+			myDataAvailable{true};
 			}
 
-			/**
-			 * @brief recives commands/data
-			 * 			
-			 *
-			 */
-			void recivedData() noexcept override
-			 {
-				
-				return 0U; // placeholder
-			 }
+		/**
+		 * @brief recives commands/
+		 *
+		 * @param[out] buf Buffer holding received data.
+		 * @param[in] bufLen Buffer length in bytes.
+		 *
+		 * @return Number of received bytes.
+		 *
+		 */
+		std::uint16_t received(std::uint8_t *buf, std::uint16_t bufLen) noexcept override
+		{
+			//Checks if there is a msg/data available.
+			if (!myDataAvailable) { return 0U};
+			std::printf("Message recived:\n")
+			std::printf("%s",msg);
+			myDataAvailable{false};
+		
+		}
 
-			 /**
-			  * @brief recivde command.
-			  * 
-			  * 	The device will responed on the command sent to it.
-			  */
-			 void reciveCommand() noexcept
-			 {
+		/**
+		 * @brief connected
+		 *
+		 * @return true
+		 * @return false
+		 */
+		bool isConnected() const noexcept override
+		{
+			return myConnetionStatus;
+		}
 
-				if (!availableCommand) {return 0U;} // Check if there is even a message/command sent.
+		/**Stub construct Forbidden moves/copy. **/
+		Stub(const Stub &) = delete;			// No copy constructor.
+		Stub(Stub &&) = delete;					// No move constructor.
+		Stub &operator=(const Stub &) = delete; // No copy assignment.
+		Stub &operator=(Stub &&) = delete;		// No move assignment.
 
-				return 0U; // Placeholder
-                             
-                           
-			}
+	private:
+		/** Buffer size. */
+		static constexpr std::uint16_t BufSize{100U};
+		std::uint8_t myDataMsg[BufSize]; // Data array
+		std::char* msg;
 
-			/**
-			 * @brief connected
-			 *
-			 * @return true
-			 * @return false
-			 */
-			 bool connected() noexcept override
-			 {
-				std::printf("Connected!\n")
-			 }
-
-			/**
-			 * @brief disconnected
-			 *
-			 * @return true
-			 * @return false
-			 */
-			bool disconnected() noexcept override
-			{
-
-				std::printf("Disconnected!\n")
-			}
-
-			/**Stub construct Forbidden moves/copy. **/
-			Stub(const Stub&) 				= delete; // No copy constructor.
-			Stub(Stub &&) 					= delete; // No move constructor.
-			Stub &operator=(const Stub &)	= delete; // No copy assignment.
-			Stub &operator=(Stub &&) 		= delete; // No move assignment.
-
-			private:
-
-			 /** Buffer size. */
-    		static constexpr std::uint8_t BufSize{100U};
-
-
-			std:uint8_t data[BufSize]; // Data array 
-
-
-			bool availableCommand; // Status if there is a command.
-			bool connectionStatus; // status off the UART connection 
-        }
+		bool myDataAvailable;	// Status if there is a command.
+		bool myConnetionStatus; // status off the UART connection
+	}
 
 } // namespace include::driver::serial
-	
