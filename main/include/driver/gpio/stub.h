765812f2244910d3c36978050996f3e36500a2a2
alexander <alexander.hassiakis@gmail.com>
2026-04-27 11:18:23 +0200

    started with stub/gpio

--- main/include/driver/gpio/stub.h
+++ main/include/driver/gpio/stub.h
@@ -1,1 +1,43 @@
 #pragma once
+
+namespace include::driver::gpio
+{
+	class Interface
+	{
+	public:
+		/**
+		 * @brief Destructor.
+		 */
+		 ~Stub() noexcept = default;
+
+		/**
+		 * @brief Construct a new Stub object
+		 * 
+		 */
+		 Stub() noexcept = default;
+
+		/**
+		 * @brief Turns output the Gpio_pin.
+		 * @param [in] GPIO pinnumber.
+		 */
+		 void output(std::uint8_t pinNumber) noexcept override;
+
+		/**
+		 * @brief Turns input the Gpio_pin.
+		 * @param [in] GPIO pinnumber.
+		 */
+		 void input(std::uint8_t pinNumber) noexcept override;
+
+		/**
+		 * @brief Turns on Toggle
+		 * @param [in] GPIO pinnumber.
+		 */
+		 void toggle(std::uint8_t pinNumber) noexcept override;
+
+		/**
+		 * @brief Pullup resistor
+		 * @param [in] GPIO pinnumber.
+		 */
+		 void pullUpGpio(std::uint8_t pinNumber) noexcept override;
+	}
+}
