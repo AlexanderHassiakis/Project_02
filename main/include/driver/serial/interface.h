3fc30989969b3229afecfd55874a4a158134249c
alexander <alexander.hassiakis@gmail.com>
2026-04-27 10:31:28 +0200

    added new Stub code

--- main/include/driver/serial/interface.h
+++ main/include/driver/serial/interface.h
@@ -4,57 +4,51 @@
 
 namespace include::driver
 {
-	class Interface
-	{
+class Interface
+{
+public:
 
-		public:
-
-		/**
-		 * @brief destructor
-		 * 			Deletes to release memory
-		 */
-		virtual ~Interface() noexcept = default:
-
-
-		/**
-		 * @brief Initializes the Uart communication
-		 * 
-		 */
-		virtual void init_uart() noexcept = 0;
-
-		/**
-		 * @brief sending commands/data
-		 * 
-		 */
-		virtual void send_data() noexcept = 0;
+	/**
+	 * @brief destructor
+	 * 			Deletes to release memory
+	 */
+	virtual ~Interface() noexcept = default:
 
 
-		/**
-		 * @brief recives commands/data
-		 * 
-		 */
-		virtual void recivedData() noexcept = 0;
+	/**
+	 * @brief Initializes the Uart communication
+	 * 
+	 * @return True on success, false on
+	 * 
+	 */
+	virtual bool init() noexcept = 0;
 
-		/**
-		 * @brief connected
-		 * 
-		 * @return true 
-		 * @return false 
-		 */
-		virtual bool connected() noexcept = 0;
+	/**
+	 * @brief sending commands/data
+	 * 
+	 */
+	virtual void send(const char* msg) noexcept = 0;
 
-		/**
-		 * @brief disconnected
-		 * 
-		 * @return true 
-		 * @return false 
-		 */
-		virtual bool disconnected() noexcept = 0;
+	virtual void send(const std::uint8_t* buf, std::uint16_t bufLen) noexcept = 0;
 
 
+	/**
+	 * @brief recives commands/
+	 * 
+	 * @param[out] buf Buffer holding received data.
+	 * @param[in] bufLen Buffer length in bytes.
+	 * 
+	 * @return Number of received bytes.
+	 * 
+	 */
+	virtual std::uint16_t received(std::uint8_t* buf, std::uint16_t bufLen) noexcept = 0;
 
-
-
-	}
-	
+	/**
+	 * @brief connected
+	 * 
+	 * @return true 
+	 * @return false 
+	 */
+	virtual bool isConnected() const noexcept = 0;
+};
 } // namespace include::driver::interface
