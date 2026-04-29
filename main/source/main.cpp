4f3dd8a4e3232d74f7bdaec767e7ef14d33d713e
alexander <alexander.hassiakis@gmail.com>
2026-04-29 20:58:46 +0200

    testing the gpio driver

--- main/source/main.cpp
+++ main/source/main.cpp
@@ -4,32 +4,41 @@
  */
 #include <cstdint>
 
-#include "driver/gpio.h"
-#include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "driver/gpio/esp32s3.h"
 
-/**
- * @brief Blink an LED every 500 ms.
- */
+
+driver::gpio::Esp32s3 myGpio;
+
+constexpr std::uint8_t LED_PIN_1 = 1;
+constexpr std::uint8_t LED_PIN_2 = 2;
+
+constexpr std::uint32_t blinkSpeed_ms{500U};
+
+// Blink an LED every 500 ms.
+
 extern "C" void app_main(void) {
-  constexpr gpio_num_t ledPin{static_cast<gpio_num_t>(2U)};
-  constexpr std::uint32_t gpioHigh{1U};
-  constexpr std::uint32_t gpioLow{0U};
-  constexpr std::uint32_t blinkSpeed_ms{500U};
 
-  // Configure LED as output.
-  gpio_reset_pin(ledPin);
-  gpio_set_direction(ledPin, GPIO_MODE_OUTPUT);
+  // Configure LED 1 & 2 as output.
+  myGpio.output(LED_PIN_1,true);
+  myGpio.output(LED_PIN_2,false);
+
+  
+
+  
 
   // Blink LED every 500 ms.
   while (1) {
-    gpio_set_level(ledPin, gpioHigh);
-    ESP_LOGI("MAIN", "LED on!");
+    
+    myGpio.toggle(LED_PIN_1);
+    myGpio.toggle(LED_PIN_2);
+    std::printf("Toggel leds!\n");
     vTaskDelay(pdMS_TO_TICKS(blinkSpeed_ms));
 
-    gpio_set_level(ledPin, gpioLow);
-    ESP_LOGI("MAIN", "LED off!");
+    myGpio.toggle(LED_PIN_1);
+    myGpio.toggle(LED_PIN_2);
+    std::printf("Toggel leds!\n")
     vTaskDelay(pdMS_TO_TICKS(blinkSpeed_ms));
   }
 }
