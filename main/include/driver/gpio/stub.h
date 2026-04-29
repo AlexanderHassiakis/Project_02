54cc1e00c178f949933b84053b87bc7963cf73f2
S-Qasim <suhaib_19@hotmail.com>
2026-04-29 15:34:35 +0200

    Add timer driver

---
+++ main/include/driver/gpio/stub.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <cstdint>
+#include <cstdio>
+
+#include "driver/gpio.h"
+#include "driver/gpio/interface.h"
+
+namespace include::driver::gpio
+    
+    class Interface
+    {
+    public:
+    /**
+     * @brief Destructor.
+     */
+    ~Stub() noexcept = default;
+    }
+
+    /**
+     * @brief Construct a new Stub object.
+     * 
+     */
+
+    Stub() noexcept = default;
+
+    /**
+     * @brief Turn output the GPIO_pin
+     * 
+     * @param [in] GPIO pinnumber.
+     */
+
---
+++ main/include/driver/timer/esp32s3.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "driver/timer/interface.h"
+#include <cstdint>
+
+#include <esp_timer.h>
+
+namespace driver::timer
+{
+    class Esp32s3 : public Interface
+    {
+        public:
+            Esp32s3() noexcept;
+            ~Esp32s3() noexcept override;
+
+            void start() noexcept override;
+            void stop() noexcept override;
+            void setPeriod(std::uint32_t period_ms) noexcept override;
+            bool hasExpired() const noexcept override;
+            bool isInitialized() const noexcept override;
+
+        private:
+            bool is_initialized_;
+            bool is_running_;
+            std::uint32_t period_ms_;
+            int64_t start_time_us_; 
+    };
+
+} // namespace driver::timer
+
+ // 64-bitars int används för att undvika overflow (32-bitar) Databladet använder 52-bitar hårdvaruregister,
+ // mjukvaran läser av som 64-bitar.
