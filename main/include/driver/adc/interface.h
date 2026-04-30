d05c48e914e228fc12aa68f754dc54ed58aee630
Fadi lazkani <fadulcalaz@gmail.com>
2026-04-30 15:01:05 +0200

    stucture for adc stub and interface

--- main/include/driver/adc/interface.h
+++ main/include/driver/adc/interface.h
@@ -1,6 +1,18 @@
 #pragma once
 
+#include <cstdint>
 
+namespace driver::adc
+{
+class Interface 
+{
+public:
+    virtual ~Interface() noexcept= default;
 
+    virtual bool isInitialized() const noexcept = 0;
 
+    virtual std::uint16_t readRaw(std::uint8_t pin) noexcept = 0;
 
+    virtual float readVoltage(std::uint8_t pin) noexcept = 0;
+};
+} // namespace driver::adc
---
+++ main/include/driver/adc/stub.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <cstdint>
+
+#include "driver/adc/interface.h"
+
+namespace driver::adc
+{
+class Stub final : public Interface 
+{
+public:
+    explicit Stub(const std::uint16_t adcMax = DefaultAdcMax,
+                  const float supplyVoltage = DefaultVoltage) noexcept
+        // Check if supply voltage is >= 0.0, if so, use default value 3.3
+        : mySupplyVoltage{0.0 < supplyVoltage ? supplyVoltage : DefaultVoltage}
+        // Check if adcMax is 0, if so, use default value 4095.
+        , myAdcMax{0U != adcMax ? adcMax : DefaultAdcMax}
+        , myRawValue{}
+    {}
+
+     bool isInitialized() const noexcept override
+     {
+        // Return true;
+     }
+
+     std::uint16_t readRaw(std::uint8_t pin) noexcept override
+     {
+        // Pin number is not applicable for stubs, ignore.
+        (void) (pin);
+        return myRawValue;
+     }
+
+     std::float readVoltage(std::uint8_t pin) noexcept override
+     {
+        // Pin number is not applicable for stubs, ignore.
+        (void) (pin);
+
+        // Calulate raw value ration (0.0-1.0).
+        const float ratio{static_cast<float>(myRawValue) / myAdcMax};
+
+        // Voltage = raw value / ADC max * supply voltage, ensure floating-point division.
+        return ratio * mySupplyVoltage;
+     }
+
+     // voltage = raw / ADC_MAX.
+
+    void simulateRaw(const std::uint16_t rawValue) noexcept 
+    { 
+        // Check given raw value, ignore if above the max limit.
+        if (myAdcMax < rawValue) { return; }
+        myRawValue = rawValue; 
+    }
+
+private:
+     static constexpr float DefaultVoltage{3.3};
+     static constexpr std::uint16_t DefaultAdcMax{4095U};
+
+     /** Supply voltage. */
+     const float mySupplyVoltage;
+
+     /** Max ADC value (used for voltage calculations). */
+     const std::uint16_t myAdcMax;
+
+     /** Simulated raw value. */
+     std::uint16_t myRawValue;
+};
+} // namespace driver::adc
+
+
