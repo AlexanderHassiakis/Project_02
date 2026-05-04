19c3d71bfc0b02b55c083bc8a0040058088ed3d3
alexander <alexander.hassiakis@gmail.com>
2026-05-04 13:16:55 +0200

    staring with Logic branch

---
+++ main/include/system/logic/logic.h
@@ -0,0 +1,43 @@
+
+/**
+ * @brief Logic driver 
+ * 
+ */
+
+
+
+namespace system::logic
+{
+	class logic final
+	{
+	
+		public:
+			explicit logic(/* args */) noexcept
+			:
+			{}
+
+			~logic() noexcept
+			{
+
+			}
+
+
+
+
+
+
+
+  	Logic()                        = delete;
+    Logic(const Logic&)            = delete;
+    Logic(Logic&&)                 = delete;
+    Logic& operator=(const Logic&) = delete;
+    Logic& operator=(Logic&&)      = delete;
+
+
+	private:
+
+
+
+	};
+} // namespace system::logic
+
