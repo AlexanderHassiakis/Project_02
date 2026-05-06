d46f70ca44b820cd6626ceb104d930bf78249a45
alexander <alexander.hassiakis@gmail.com>
2026-05-06 20:15:32 +0200

    Removed old code

--- main/include/driver/tempsensor/tmp36.h
+++ main/include/driver/tempsensor/tmp36.h
@@ -7,33 +7,27 @@
 namespace driver::tempsensor {
 
 class Tmp36 final : public Interface {
-public:
-  /**
-   * @brief Construct a new Tmp36 object
-   * @param channel ADC-kanalen (t.ex. ADC_CHANNEL_0 för GPIO 1)
-   */
-  explicit Tmp36(
-      adc_channel_t channel) noexcept; // Tog bort override, lade till parameter
+    public:
+        /**
+         * @brief Construct a new Tmp36 object
+         * @param channel ADC-kanalen (t.ex. ADC_CHANNEL_0 för GPIO 1)
+         */
+        explicit Tmp36() noexcept; 
 
-  /**
-   * @brief Destroy the Tmp36 object
-   */
-  ~Tmp36() noexcept override;
+        /**
+         * @brief Destroy the Tmp36 object
+         */
+        ~Tmp36() noexcept override;
 
-  /**
-   * @brief Reads temperature in deci-Celsius (225 = 22.5C)
-   * @return int
-   */
-  int readTemperature() noexcept override;
+        /**
+         * @brief Reads temperature in deci-Celsius (225 = 22.5C)
+         * @return int
+         */
+        int readTemperature() noexcept override;
 
-private:
-  adc_channel_t m_channel;
-  adc_oneshot_unit_handle_t m_adc_handle;
+    private:
 
-  /**
-   * @brief Initierar ADC-hårdvaran
-   */
-  void initAdc() noexcept;
-};
+        void initAdc() noexcept;
+    };
 
 } // namespace driver::tempsensor
--- main/source/driver/tempsensor/tmp36.cpp
+++ main/source/driver/tempsensor/tmp36.cpp
@@ -5,32 +5,22 @@
 #include "esp_log.h"
 
 #include "driver/tempsensor/tmp36.h"
+#include "driver/adc/esp32s3.h"
 
-/**
- * @brief Guide
- *
- * ADC_CHANNEL_0 = GPIO 1
- * ADC_CHANNEL_1 = GPIO 2
- * ADC_CHANNEL_2 = GPIO 3
- * ADC_CHANNEL_3 = GPIO 4
- *
- */
+
 
 namespace driver::tempsensor {
 
     Tmp36::Tmp36(adc_channel_t channel) noexcept
         : m_channel(channel), m_adc_handle(nullptr) 
     {
-        initAdc();
-        ESP_LOGI("TMP36", "ADC-resurser Skapade.");
+      adc::esp32s3::Esp32s3();
+      ESP_LOGI("TMP36", "ADC-resurser Skapade.");
     }
 
     Tmp36::~Tmp36() {
-    if (m_adc_handle != nullptr) 
-    {
-        adc_oneshot_del_unit(m_adc_handle);
-        ESP_LOGI("TMP36", "ADC-resurser frigjorda.");
-    }
+        adc::esp32s3::~Esp32s3();
+        ESP_LOGI("TMP36", "ADC-resurser Skapade.");
     } 
 
     int Tmp36::readTemperature() noexcept {
@@ -39,8 +29,7 @@ namespace driver::tempsensor {
 
     for (uint8_t i = 0; i < 8; ++i) {
         int val = 0;
-        // Glöm inte: lägg till '&' före val
-        adc_oneshot_read(m_adc_handle, m_channel, &val);
+        val += Adc::Esp32s3::readRaw(1);
         sum += val;
     }
 
@@ -54,28 +43,6 @@ namespace driver::tempsensor {
     // Returnerar tiondels grader (t.ex. 225 för 22.5°C)
     return (voltage_mv - 500);
   
-    }
-
-    void Tmp36::initAdc() noexcept {
-        adc_oneshot_unit_init_cfg_t init_config = {};
-        init_config.unit_id = ADC_UNIT_1;  //används oftast för GPIO 1-10.
-        init_config.ulp_mode = ADC_ULP_MODE_DISABLE;
-
-        esp_err_t err = adc_oneshot_new_unit(&init_config, &m_adc_handle); 
-        if (err != ESP_OK) {
-            ESP_LOGE("TMP36", "Misslyckades att initiera ADC: %s", esp_err_to_name(err));
-            return;
-        }
-
-        adc_oneshot_chan_cfg_t config = {};
-        config.bitwidth = ADC_BITWIDTH_DEFAULT;
-        config.atten = ADC_ATTEN_DB_12; // Dämpar signalen så vi kan mäta upp till ca 3.3V
-
-        err = adc_oneshot_config_channel(m_adc_handle, m_channel, &config);
-        if (err != ESP_OK) {
-            ESP_LOGE("TMP36", "Misslyckades att konfigurera ADC-kanal: %s", esp_err_to_name(err));
-        } 
-
     }
 
 } // namespace driver::tempsensor
