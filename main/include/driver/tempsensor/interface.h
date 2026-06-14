72de1fa4c3bcd35c6b0d98905b13d97a123c7d30
Erik Pihl <erik.axel.pihl@gmail.com>
2026-06-14 10:41:41 +0200

    Review tempsensor files

--- main/include/driver/tempsensor/interface.h
+++ main/include/driver/tempsensor/interface.h
@@ -1,28 +1,29 @@
+//! @note File header missing.
 #pragma once
 
 namespace driver::tempsensor
 {
-    class Interface
-    {
+//! @note Class header missing.
+class Interface
+{
+public:
+    /**
+     * @brief Destroy the Interface object
+     *
+     */
+    //! @note Added noexcept.
+    virtual ~Interface() noexcept = default;
 
-    public:
-        /**
-         * @brief Destroy the Interface object
-         *
-         */
-        virtual ~Interface() = default;
+    /**
+     * @brief Reads the value from the ADC and converts it to celcius.
+     *
+     * @return int ??
+     */
+    virtual int readTemperature() noexcept = 0;
 
-        /**
-         * @brief Reads the value from the ADC and converts it to celcius.
-         *
-         * @return int
-         */
-        virtual int readTemperature() noexcept = 0;
-
-        /**
-         * @brief Construct a new init Temp object
-         *
-         */
-        virtual void initTemp() noexcept = 0;
-    };
+    /**
+     * @brief Construct a new init Temp object
+     */
+    virtual void initTemp() noexcept = 0;
+};
 } // namespace driver::tempsensor
