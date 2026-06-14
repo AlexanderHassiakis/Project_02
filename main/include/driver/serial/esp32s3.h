52a17eb305b2478f3e767eccc1482a70491aa82d
Erik Pihl <erik.axel.pihl@gmail.com>
2026-06-14 10:30:08 +0200

    Review serial files

--- main/include/driver/serial/esp32s3.h
+++ main/include/driver/serial/esp32s3.h
@@ -1,11 +1,15 @@
+//! @note File header missing.
 #pragma once
 
-#include <cstdint>	// Int variavber
+//! @note Sort headers: standard library before project headers.
+#include <cstdint>
 
 #include "driver/serial/interface.h"
 
+//! @note Bracket on the next line, please.
 namespace driver::serial
 {
+//! @note Class header missing.
 class Esp32s3 final : public Interface
 {
 public:
@@ -15,31 +19,30 @@ public:
 	Esp32s3() noexcept;
 
 	/**
-	 * @brief Destructor
-	 * 			Deletes to release memory
+	 * @brief Destructor.
 	 */
-	~Esp32s3() noexcept;
+	//! @note Added override.
+	~Esp32s3() noexcept override;
 
 	/**
 	 * @brief Initialize the Uart communication
 	 *
 	 * @return True on success, false on failure.
-	 *
 	 */
 	bool init() noexcept override;
 
 	/**
 	 * @brief Construct a new send string.
 	 * 
-	 * @param msg 
+	 * @param msg ??
 	 */
 	void send(const char *msg) noexcept override;
 
 	/**
 	 * @brief Send bytes.
 	 * 
-	 * @param buf 
-	 * @param bufLen 
+	 * @param buf ??
+	 * @param bufLen ??
 	 */
 	void send(const std::uint8_t *buf, std::uint16_t bufLen) noexcept override;
 
@@ -66,12 +69,11 @@ public:
         /**
 	 * @brief connected
 	 *
-	 * @return true
+	 * @return true ??
 	 * @return false
 	 */
 	bool isConnected() const noexcept override;
 
-	/**Esp32s3 construct Forbidden moves/copy. **/
 	Esp32s3(const Esp32s3 &)            = delete; // No copy constructor.
 	Esp32s3(Esp32s3 &&)                 = delete; // No move constructor.
 	Esp32s3 &operator=(const Esp32s3 &) = delete; // No copy assignment.
@@ -79,11 +81,15 @@ public:
 
 private:
 	/** Buffer size. */
-	static constexpr std::uint16_t BufSize{200U}; // Array size.
-	std::uint8_t myDataMsg[BufSize]{0U};		  // Data array
-	const char *myMsg;
+	static constexpr std::uint16_t BufSize{200U};
 
-	bool myDataAvailable;	 // Status if there is a command.
-	bool myConnectionStatus; // status off the UART connection
+	//! @note Don't initialize members here, please do it in the constructor.
+	//! All of them I mean. I fixed it for you this time. :)
+
+	//! @note Also try to comment the member variables.
+	std::uint8_t myDataMsg[BufSize];
+	const char *myMsg;
+	bool myDataAvailable;
+	bool myConnectionStatus;
 };
 } // namespace driver::serial
--- main/include/driver/serial/interface.h
+++ main/include/driver/serial/interface.h
@@ -1,20 +1,21 @@
+//! @note File header missing.
 #pragma once
 
 #include <cstdint>
 
 namespace driver::serial
 {
+//! @note Class header missing.
 class Interface
 {
 public:
-
 	/**
 	 * @brief destructor
 	 * 			Deletes to release memory
 	 */
 	virtual ~Interface() noexcept = default;
 
-
+	//! @note Write imperative comments, like "Initialize the ..." instead of "Initializes".
 	/**
 	 * @brief Initializes the Uart communication
 	 * 
@@ -27,8 +28,10 @@ public:
 	 * @brief sending commands/data
 	 * 
 	 */
+	//! @note Missing param documentation.
 	virtual void send(const char* msg) noexcept = 0;
 
+	//! @note Missing documentation.
 	virtual void send(const std::uint8_t* buf, std::uint16_t bufLen) noexcept = 0;
 
 
@@ -41,6 +44,9 @@ public:
 	 * @return Number of received bytes.
 	 * 
 	 */
+	//! @note Good call to use the same comment for the overloaded methods here. But if you do,
+	//! please ensure that the input arguments are called the same. How about buf and bufLen
+	//! for both?
 	virtual std::uint16_t received(std::uint8_t* buf, std::uint16_t bufLen) noexcept = 0;
 	virtual std::uint16_t received(const char *str, std::uint16_t strLen) noexcept = 0;
 
@@ -50,6 +56,7 @@ public:
 	 * @return true 
 	 * @return false 
 	 */
+	//! @note Specificy what is returned.
 	virtual bool isConnected() const noexcept = 0;
 };
 } // namespace driver::serial
