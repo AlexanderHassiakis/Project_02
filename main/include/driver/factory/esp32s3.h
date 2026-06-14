e52b96b270c1240483f432bc1f5090c67cabd6f7
Erik Pihl <erik.axel.pihl@gmail.com>
2026-06-14 09:51:06 +0200

    Review factory files

--- main/include/driver/factory/esp32s3.h
+++ main/include/driver/factory/esp32s3.h
@@ -1,23 +1,31 @@
+/**
+ * @brief ESP32-S3 factory.
+ */
 #pragma once
 
 #include <cstdint>
 #include <memory>
 
-#include "driver/factory/interface.h"
 #include "driver/adc/esp32s3.h"
+#include "driver/factory/interface.h"
 #include "driver/gpio/esp32s3.h"
+#include "driver/mqtt/esp32s3.h"
 #include "driver/serial/esp32s3.h"
 #include "driver/timer/esp32s3.h"
 #include "driver/tempsensor/tmp36.h"
 #include "driver/watchdog/esp32s3.h"
-#include "driver/mqtt/esp32s3.h"
 
 namespace driver::factory 
 {
+/**
+ * @brief ESP32-S3 factory.
+ * 
+ *        This class should be non-copyable and non-movable.
+ */
 class Esp32s3 final : public Interface 
 {
 public:
-
+    //! @note Doxygen comments missing!
     ~Esp32s3() noexcept override = default;
 
     std::unique_ptr<adc::Interface> adc(std::uint8_t pin) noexcept override;
@@ -33,7 +41,7 @@ public:
     std::unique_ptr<watchdog::Interface> watchdog() noexcept override;
 
     std::unique_ptr<mqtt::Interface> mqtt() noexcept override;
-    
-};
 
+    //! @note Please prevent copy and move operations here!
+};
 } //namespace driver::factory
