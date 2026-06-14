f98cb9d2837371bb987c806bc48f4f686e204051
Erik Pihl <erik.axel.pihl@gmail.com>
2026-06-14 10:52:04 +0200

    Review timer files

--- main/include/driver/timer/esp32s3.h
+++ main/include/driver/timer/esp32s3.h
@@ -1,5 +1,7 @@
+//! @note File header missing.
 #pragma once
 
+//! @note Sort headers. Include standard library before project headers.
 #include "driver/timer/interface.h"
 #include <cstdint>
 
@@ -7,55 +9,61 @@
 
 namespace driver::timer
 {
-    class Esp32s3 : public Interface
-    {
-        public:
-            Esp32s3() noexcept;
-            ~Esp32s3() noexcept override;
+//! @note Class header missing.
+//! @note Add final.
+class Esp32s3 : public Interface
+{
+public:
+    //! @note Doxygen comments missing for these.
+    Esp32s3() noexcept;
+    ~Esp32s3() noexcept override;
 
-            /**
-             * @brief Start timer.
-             * 
-             * setPeriod use set period for timer length. 
-             */
-            void start() noexcept override;
-            /**
-             * @brief Stops the timer.
-             */
-            void stop() noexcept override;
-            /**
-             * @brief Sets the timeout period.
-             *
-             * @param period_ms Period in milliseconds.
-             */
-            void setPeriod(std::uint32_t period_ms) noexcept override;
-            /**
-             * @brief Checks if the timer has reached.
-             *
-             * @return true if timeout, false if not.
-             */
-            bool hasExpired() const noexcept override;
-            /**
-             * @brief Checks if the timer driver is initialized.
-             *
-             * @return true if initialized, false if not initialized.
-             */
-            bool isInitialized() const noexcept override;
+    /**
+     * @brief Start timer.
+     *
+     * setPeriod use set period for timer length.
+     */
+    void start() noexcept override;
 
-            /**Esp32s3 construct Forbidden moves/copy. **/
-            Esp32s3(const Esp32s3 &) = delete;
-            Esp32s3(Esp32s3 &&) = delete;
-            Esp32s3 &operator=(const Esp32s3 &) = delete;
-            Esp32s3 &operator=(Esp32s3 &&) = delete;
+    /**
+     * @brief Stops the timer.
+     */
+    void stop() noexcept override;
 
-        private:
-            bool is_initialized_;
-            bool is_running_;
-            std::uint32_t period_ms_;
-            int64_t start_time_us_; 
-    };
+    /**
+     * @brief Sets the timeout period.
+     *
+     * @param period_ms Period in milliseconds.
+     */
+    void setPeriod(std::uint32_t period_ms) noexcept override;
 
+    /**
+     * @brief Checks if the timer has reached.
+     *
+     * @return true if timeout, false if not.
+     */
+    bool hasExpired() const noexcept override;
+
+    /**
+     * @brief Checks if the timer driver is initialized.
+     *
+     * @return true if initialized, false if not initialized.
+     */
+    bool isInitialized() const noexcept override;
+
+    /**Esp32s3 construct Forbidden moves/copy. **/
+    Esp32s3(const Esp32s3 &) = delete;
+    Esp32s3(Esp32s3 &&) = delete;
+    Esp32s3 &operator=(const Esp32s3 &) = delete;
+    Esp32s3 &operator=(Esp32s3 &&) = delete;
+
+private:
+    //! @note Please use prefix 'my' for member variables instead of postfix _.
+    bool is_initialized_;
+    bool is_running_;
+    std::uint32_t period_ms_;
+
+    //! @note Please use std::int64_t instead of int64_t.
+    int64_t start_time_us_; 
+};
 } // namespace driver::timer
-
- // 64-bitars int används för att undvika overflow (32-bitar) Databladet använder 52-bitar hårdvaruregister,
- // programmet läser av som 64-bitar.
